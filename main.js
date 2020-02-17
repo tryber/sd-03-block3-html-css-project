1941e7724f38c222ce15703bd5d9da85c2180794
Cezene <cezenesn@hotmail.com.br>
2020-02-17 13:44:52 -0300

    identacao

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 $('.slider-principal').slick({ 
-    
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     autoplay: true,
     autoplayspeed: 5000
-
-
 });
 $('.cast-img').slick({ 
-    
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     autoplay: true,
     autoplayspeed: 2000
-
-
 });
 
