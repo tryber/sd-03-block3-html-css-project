26d22ce4e931893f4ad0b72efe0625ddffc243e0
Cezene <cezenesn@hotmail.com.br>
2020-02-17 09:20:22 -0300

    donnie darko site

---
+++ main.js
@@ -0,0 +1,21 @@
+$('.slider-principal').slick({ 
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplayspeed: 5000
+
+
+});
+$('.cast-img').slick({ 
+    
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplayspeed: 2000
+
+
+});
+
